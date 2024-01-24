7bfb109c6e429e301a9e38534ab03ba118bcf9ff
joaobug <joaoenfrance@gmail.com>
2024-01-24 17:33:29 -0300

    parse json

--- learningApp/learningAppApp.swift
+++ learningApp/learningAppApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct learningAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
